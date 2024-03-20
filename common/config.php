dd4e61adf99e8ba8dc9e5da54a83d5a44c5c4e10
Darpan Patel <exegil-sw-30@gujarat.gov.in>
2024-03-20 17:50:35 +0530

    smtp

--- common/config.php
+++ common/config.php
@@ -2,7 +2,7 @@
 // DB credentials.
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','pwd');
+define('DB_PASS','');
 define('DB_NAME','travel');
 // Establish database connection.
 try
